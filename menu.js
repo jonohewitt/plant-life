6ec313e775c1a096c31af0f8e20182dad0ee7ede
Jono Hewitt <jonohewitt@gmail.com>
2020-08-14 12:33:53 +0100

    Improve accessibility using button element and tabIndex

--- menu.js
+++ menu.js
@@ -1,12 +1,15 @@
-const toggleTag = document.querySelector("div.toggle-nav");
+const toggleTag = document.querySelector("button.toggle-nav");
 const mainTag = document.querySelector("main");
+const settingsOptionTags = document.querySelectorAll(".settingsOption");
 
 toggleTag.addEventListener("click", () => {
   mainTag.classList.toggle("open");
 
   if (mainTag.classList.contains("open")) {
-    toggleTag.innerHTML = `<img src="assets/close.svg" alt="close icon"> Close`
+    toggleTag.innerHTML = `<img src="assets/close.svg" alt="close icon"> Close`;
+    settingsOptionTags.forEach(tag => tag.setAttribute("tabindex", "0"));
   } else {
     toggleTag.innerHTML = `<img src="assets/menu.svg" alt="menu icon"> Menu`;
+    settingsOptionTags.forEach(tag => tag.setAttribute("tabindex", "-1"));
   }
-})
+});
